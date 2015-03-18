3c937a422cc94f0f914053bf2122d52f80526a59
kszymans <przypieczony@gmail.com>
2015-03-18 23:46:00 +0100

    Created new version of GUI v.03, added new icons

--- GUIv0.3.py
+++ GUIv0.3.py
@@ -2,7 +2,7 @@
 
 # Form implementation generated from reading ui file 'GUI.ui'
 #
-# Created: Tue Mar 17 18:43:26 2015
+# Created: Tue Mar 17 20:30:38 2015
 #      by: PyQt4 UI code generator 4.10.4
 #
 # WARNING! All changes made in this file will be lost!
@@ -10,6 +10,7 @@
 from PyQt4 import QtCore, QtGui
 import socket
 import random
+import sys
 
 try:
     _fromUtf8 = QtCore.QString.fromUtf8
@@ -28,7 +29,7 @@ except AttributeError:
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
         MainWindow.setObjectName(_fromUtf8("MainWindow"))
-        MainWindow.resize(695, 528)
+        MainWindow.resize(740, 551)
         self.centralwidget = QtGui.QWidget(MainWindow)
         self.centralwidget.setObjectName(_fromUtf8("centralwidget"))
         self.gridLayout = QtGui.QGridLayout(self.centralwidget)
@@ -70,7 +71,7 @@ class Ui_MainWindow(object):
         self.sourcePortField.setStatusTip(_fromUtf8(""))
         self.sourcePortField.setInputMethodHints(QtCore.Qt.ImhPreferNumbers)
         self.sourcePortField.setInputMask(_fromUtf8(""))
-        self.sourcePortField.setText(_fromUtf8("6661"))
+        self.sourcePortField.setText(_fromUtf8("6666"))
         self.sourcePortField.setObjectName(_fromUtf8("sourcePortField"))
         self.verticalLayout.addWidget(self.sourcePortField)
         self.label_3 = QtGui.QLabel(self.layoutWidget)
@@ -90,7 +91,7 @@ class Ui_MainWindow(object):
         self.destPortField.setStatusTip(_fromUtf8(""))
         self.destPortField.setInputMethodHints(QtCore.Qt.ImhPreferNumbers)
         self.destPortField.setInputMask(_fromUtf8(""))
-        self.destPortField.setText(_fromUtf8("6662"))
+        self.destPortField.setText(_fromUtf8("6666"))
         self.destPortField.setObjectName(_fromUtf8("destPortField"))
         self.verticalLayout.addWidget(self.destPortField)
         self.layoutWidget1 = QtGui.QWidget(self.Inputdata)
@@ -105,18 +106,70 @@ class Ui_MainWindow(object):
         self.acceptButton = QtGui.QPushButton(self.layoutWidget1)
         self.acceptButton.setObjectName(_fromUtf8("acceptButton"))
         self.horizontalLayout.addWidget(self.acceptButton)
-        self.sipTemplate = QtGui.QTextEdit(self.Inputdata)
-        self.sipTemplate.setGeometry(QtCore.QRect(300, 90, 321, 261))
+        self.splitter = QtGui.QSplitter(self.Inputdata)
+        self.splitter.setGeometry(QtCore.QRect(280, 40, 371, 331))
+        self.splitter.setOrientation(QtCore.Qt.Vertical)
+        self.splitter.setObjectName(_fromUtf8("splitter"))
+        self.label_5 = QtGui.QLabel(self.splitter)
+        self.label_5.setObjectName(_fromUtf8("label_5"))
+        self.sipTemplate = QtGui.QTextEdit(self.splitter)
+        font = QtGui.QFont()
+        font.setFamily(_fromUtf8("Courier New"))
+        self.sipTemplate.setFont(font)
         self.sipTemplate.setReadOnly(True)
         self.sipTemplate.setObjectName(_fromUtf8("sipTemplate"))
         self.tabWidget.addTab(self.Inputdata, _fromUtf8(""))
         self.Register = QtGui.QWidget()
         self.Register.setObjectName(_fromUtf8("Register"))
+        self.simulateButton = QtGui.QPushButton(self.Register)
+        self.simulateButton.setGeometry(QtCore.QRect(610, 410, 98, 27))
+        self.simulateButton.setObjectName(_fromUtf8("simulateButton"))
+        self.flowField = QtGui.QTextEdit(self.Register)
+        self.flowField.setGeometry(QtCore.QRect(350, 100, 213, 291))
+        self.flowField.setReadOnly(True)
+        self.flowField.setObjectName(_fromUtf8("flowField"))
+        self.flowLabel = QtGui.QLabel(self.Register)
+        self.flowLabel.setGeometry(QtCore.QRect(570, 230, 112, 17))
+        self.flowLabel.setObjectName(_fromUtf8("flowLabel"))
+        self.splitter_2 = QtGui.QSplitter(self.Register)
+        self.splitter_2.setGeometry(QtCore.QRect(30, 130, 291, 271))
+        self.splitter_2.setOrientation(QtCore.Qt.Vertical)
+        self.splitter_2.setObjectName(_fromUtf8("splitter_2"))
+        self.labelCurrentMessage = QtGui.QLabel(self.splitter_2)
+        self.labelCurrentMessage.setObjectName(_fromUtf8("labelCurrentMessage"))
+        self.currentMessageField = QtGui.QTextEdit(self.splitter_2)
+        self.currentMessageField.setReadOnly(True)
+        self.currentMessageField.setObjectName(_fromUtf8("currentMessageField"))
+        self.widget = QtGui.QWidget(self.Register)
+        self.widget.setGeometry(QtCore.QRect(40, 20, 611, 61))
+        self.widget.setObjectName(_fromUtf8("widget"))
+        self.horizontalLayout_3 = QtGui.QHBoxLayout(self.widget)
+        self.horizontalLayout_3.setMargin(0)
+        self.horizontalLayout_3.setObjectName(_fromUtf8("horizontalLayout_3"))
+        self.phone = QtGui.QLabel(self.widget)
+        self.phone.setText(_fromUtf8(""))
+        self.phone.setPixmap(QtGui.QPixmap(_fromUtf8("img/phone.jpg")))
+        self.phone.setScaledContents(True)
+        self.phone.setObjectName(_fromUtf8("phone"))
+        self.horizontalLayout_3.addWidget(self.phone)
+        self.horizontalSlider = QtGui.QSlider(self.widget)
+        self.horizontalSlider.setCursor(QtGui.QCursor(QtCore.Qt.ArrowCursor))
+        self.horizontalSlider.setAcceptDrops(False)
+        self.horizontalSlider.setAutoFillBackground(False)
+        self.horizontalSlider.setOrientation(QtCore.Qt.Horizontal)
+        self.horizontalSlider.setObjectName(_fromUtf8("horizontalSlider"))
+        self.horizontalLayout_3.addWidget(self.horizontalSlider)
+        self.server = QtGui.QLabel(self.widget)
+        self.server.setText(_fromUtf8(""))
+        self.server.setPixmap(QtGui.QPixmap(_fromUtf8("img/sip proxy server.jpg")))
+        self.server.setScaledContents(True)
+        self.server.setObjectName(_fromUtf8("server"))
+        self.horizontalLayout_3.addWidget(self.server)
         self.tabWidget.addTab(self.Register, _fromUtf8(""))
-        self.gridLayout.addWidget(self.tabWidget, 0, 0, 1, 1)
+        self.gridLayout.addWidget(self.tabWidget, 0, 1, 1, 1)
         MainWindow.setCentralWidget(self.centralwidget)
         self.menubar = QtGui.QMenuBar(MainWindow)
-        self.menubar.setGeometry(QtCore.QRect(0, 0, 695, 25))
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 740, 25))
         self.menubar.setObjectName(_fromUtf8("menubar"))
         self.menuFile = QtGui.QMenu(self.menubar)
         self.menuFile.setObjectName(_fromUtf8("menuFile"))
@@ -134,6 +187,7 @@ class Ui_MainWindow(object):
         self.menubar.addAction(self.menuFile.menuAction())
         self.menubar.addAction(self.menuEdit.menuAction())
         self.menubar.addAction(self.menuExit.menuAction())
+        self.template_vars = {}
 
         self.retranslateUi(MainWindow)
         self.tabWidget.setCurrentIndex(0)
@@ -158,7 +212,11 @@ class Ui_MainWindow(object):
         self.destPortField.setPlaceholderText(_translate("MainWindow", "e.g. 5060", None))
         self.clearButton.setText(_translate("MainWindow", "Clear", None))
         self.acceptButton.setText(_translate("MainWindow", "Accept", None))
+        self.label_5.setText(_translate("MainWindow", "SIP Template", None))
         self.tabWidget.setTabText(self.tabWidget.indexOf(self.Inputdata), _translate("MainWindow", "Input data", None))
+        self.simulateButton.setText(_translate("MainWindow", "Simulate", None))
+        self.flowLabel.setText(_translate("MainWindow", "Connection flow", None))
+        self.labelCurrentMessage.setText(_translate("MainWindow", "Current message", None))
         self.tabWidget.setTabText(self.tabWidget.indexOf(self.Register), _translate("MainWindow", "Register", None))
         self.menuFile.setTitle(_translate("MainWindow", "File", None))
         self.menuEdit.setTitle(_translate("MainWindow", "Edit", None))
@@ -196,17 +254,18 @@ class Ui_MainWindow(object):
             self.sourceAddress = random_loopback
             self.destAddress = "127.0.0.1"
 
-        template_vars = {
+        self.template_vars = {
         "source_ip": self.sourceAddress,
         "source_port": self.sourcePort,
         "dest_ip": self.destAddress,
         "dest_port": self.destPort
         }
 
-        self.sipTemplate.insertPlainText(self.printTemplate(template_vars))
+        self.sipTemplate.insertPlainText(self.printTemplate(self.template_vars))
+        self.createSockets(self.template_vars) #This is awful, it should be relocated outside this function
 
     def printTemplate(self, template_vars):
-        """Method to print text into Template box"""
+        """Method to print text into text box"""
 
         template = """OPTIONS sip:%(dest_ip)s:%(dest_port)s SIP/2.0
         Via: SIP/2.0/UDP %(source_ip)s:%(source_port)s
@@ -233,6 +292,32 @@ class Ui_MainWindow(object):
             sys.exit(-1)
         return ret
      
+    def createSockets(self, template_vars):
+        #import pdb; pdb.set_trace()
+        try:
+            self.sending_sock = self.open_sock(self.template_vars["source_ip"], self.template_vars["source_port"])
+            self.receiving_sock = self.open_sock(self.template_vars["dest_ip"], self.template_vars["dest_port"])
+            print "sockets created"
+        except Exception, e:
+            sys.stderr.write("ERROR: cannot open socket. %s\n" % (e))
+            sys.exit(-1)
+
+    def open_sock(self, ip, port):
+        try:
+            sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
+            sock.setblocking(0)
+        except Exception, e:
+            sys.stderr.write("ERROR: cannot create socket. %s\n" % e)
+            sys.exit(-1)
+        try:
+            sock.seckopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+            sock.seckopt(socket.SOL_SOCKET, socket.SO_REUSEPORT, 1)
+        except AttributeError:
+            pass
+        sock.bind((ip, port))
+        sock.settimeout(10)
+        return sock
+
     def main():
         # usage = """%prog [OPTIONS]"""
         # opt = optparse.OptionParser(usage=usage)
@@ -319,12 +404,7 @@ class Ui_MainWindow(object):
             sys.stderr.write("=======================================\n\n")
  
         count = options.count
-        try:
-            sending_sock = open_sock(options, options.source_ip, options.source_port)
-            receiving_sock = open_sock(options, options.dest_ip, options.dest_port)
-        except Exception, e:
-            sys.stderr.write("ERROR: cannot open socket. %s with address %s\n" % (e, options.source_ip))
-            sys.exit(-1)
+        
  
         sent = rcvd = ok_recvd = notify_recvd = 0 
         
@@ -371,14 +451,30 @@ class Ui_MainWindow(object):
             sys.stderr.write('%d packets transmitted, %d packets received, %.1f%% packet loss\n' % (sent, rcvd, (float(sent - rcvd) / sent) * 100))
  
  
- 
+# class MyPopup(QWidget):
+#     def __init__(self):
+#         QWidget.__init__(self)
+
+#     def paintEvent(self, e):
+#         dc = QPainter(self)
+#         dc.drawLine(0, 0, 100, 100)
+#         dc.drawLine(100, 0, 0, 100) 
+
+#     def doit(self):
+#         print "Opening a new popup window..."
+#         self.w = MyPopup()
+#         self.w.setGeometry(QRect(100, 100, 400, 200))
+#         self.w.show()
  
 if __name__ == "__main__":
-    import sys
+#try:
     app = QtGui.QApplication(sys.argv)
     MainWindow = QtGui.QMainWindow()
     ui = Ui_MainWindow()
     ui.setupUi(MainWindow)
     MainWindow.show()
+#except BaseException:
+    #raw_input("Ups.. Something unexpected happened, press any key to shutdown...")
     sys.exit(app.exec_())
  
+
